94eef44dd749c8b69eda8567a830028fe839f59f
hamza herbou <hamza95herbou@gmail.com>
2020-06-19 19:40:07 +0100

    upload files

---
+++ Assets/Shape.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+
+	Vector2[] points;
+	float strokeWidth = 0f;
+	Color fillColor = Color.white;
+	Color strokeColor = Color.black;
+
+	public static Shape Create (string shapeName)
+	{
+		return new GameObject ("Shape-" + shapeName).AddComponent <Shape> ();
+	}
+
+	public Shape SetPoints (Vector2[] shapePoints)
+	{
+		this.points = shapePoints;
+		return this;
+	}
+
+	public Shape SetStrokeWidth (float shapeStrokeWidth)
+	{
+		this.strokeWidth = shapeStrokeWidth;
+		return this;
+	}
+
+	public Shape SetStrokeColor (Color shapeStrokeColor)
+	{
+		this.strokeColor = shapeStrokeColor;
+		return this;
+	}
+
+	public Shape SetFillColor (Color shapeFillColor)
+	{
+		this.fillColor = shapeFillColor;
+		return this;
+	}
+
+	public void Draw ()
+	{
+		string color = ColorUtility.ToHtmlStringRGBA (fillColor);
+		Debug.Log ("<color=#" + color + ">Draw " + this.gameObject.name + "</color>");
+	}
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+	void Start ()
+	{
+		Shape.Create ("Default").Draw ();
+		Shape.Create ("Rectangle").SetFillColor (Color.magenta).Draw ();
+		Shape.Create ("Triangle").SetFillColor (Color.yellow).SetStrokeWidth (5f).Draw ();
+	}
+}
+
